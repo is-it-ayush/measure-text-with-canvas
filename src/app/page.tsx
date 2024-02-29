df81afd7bd30ef9449e6c482f2c7b5af8a4a1462
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-02-29 20:57:42 +0530

    chore: update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { unstable_noStore as noStore } from "next/cache";
-import { api } from "~/trpc/server";
 import { ClientWrapper } from "./clientwrapper";
 
 export default async function Home() {
